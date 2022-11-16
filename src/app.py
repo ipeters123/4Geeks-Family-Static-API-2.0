5a16c3afe41d8d139bce191bff1b03094964bc08
dariel0514 <dariel0514@hotmail.com>
2022-11-16 14:22:43 +0000

    Dn

--- src/app.py
+++ src/app.py
@@ -16,27 +16,49 @@ CORS(app)
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['GET'])
+def getOneMember(id):
+    # fill this method and update the return
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+
+@app.route('/member', methods=['POST'])
+def addNewMember():
+    # fill this method and update the return
+    request_body = json.loads(request.data)
+    jackson_family.add_member(request_body)
+    return jsonify(request_body)
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def deleteOneMember(id):
+    # fill this method and update the return
+    jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
